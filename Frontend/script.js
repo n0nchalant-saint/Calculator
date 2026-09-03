1d6b2357c5f0b0fe312279f9907c1fcf46d7d872
y9qkydddhv-prog <your_github_email@gmail.com>
2026-09-03 23:28:56 +0200

    First upload of calculator

---
+++ Frontend/script.js
@@ -0,0 +1,36 @@
+function addToDisplay(value) {
+
+    const display = document.getElementById("display");
+
+    display.value += value;
+}
+
+
+function clearDisplay() {
+
+    document.getElementById("display").value = "";
+}
+
+
+function deleteLast() {
+
+    const display = document.getElementById("display");
+
+    display.value = display.value.slice(0, -1);
+}
+
+
+function calculate() {
+
+    const display = document.getElementById("display");
+
+    try {
+
+        display.value = eval(display.value);
+
+    } catch {
+
+        display.value = "Error";
+
+    }
+}
